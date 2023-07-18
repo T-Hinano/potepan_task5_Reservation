eb9b53087a583c3ac5b7b021e3507fd02031e181
EC2 Default User <ec2-user@ip-172-31-18-45.us-east-2.compute.internal>
2023-07-18 09:52:10 +0000

    first commit

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Travelapp
+  class Application < Rails::Application
+    config.hosts << "be3e4bc27dbd41cb84979c2ef44df5cf.vfs.cloud9.us-east-2.amazonaws.com"
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
